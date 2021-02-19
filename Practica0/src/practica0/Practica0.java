ef6fd5b1444e387d186829966a86932781b2ec3d
Oscar Lista <43946678+olista94@users.noreply.github.com>
2021-02-19 10:46:02 +0100

    Add files via upload

---
+++ Practica0/src/practica0/Practica0.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica0;
+
+/**
+ *
+ * @author olista94
+ */
+public class Practica0 {
+
+    public static void intercambiarValores (int num1, int num2){
+        System.out.println("La variable 1 es: "+ num1);
+        System.out.println("La variable 2 es: "+ num2);
+        
+        int aux = num1;
+        num1=num2;
+        num2=aux;
+        
+        System.out.println("La variable 1 es: "+ num1);
+        System.out.println("La variable 2 es: "+ num2);
+    }
+    
+    public static void main(String[] args) {
+        intercambiarValores(2,3);
+    }
+    
+}
